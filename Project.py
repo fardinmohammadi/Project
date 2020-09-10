8c212ea73d71f8891f28d86f00132c47699ec9aa
FARDIN <fardinsnagian@gmail.com>
2020-09-10 08:09:41 -0400

    CREAT START CALLBACK MESSAGE

--- Project.py
+++ Project.py
@@ -1,4 +1,5 @@
-from telegram.ext import Updater,CommandHandler
+from telegram.ext import Updater,CommandHandler,CallbackQueryHandler
+from telegram import InlineKeyboardButton,InlineKeyboardMarkup
 
 
 Token = "1170278827:AAEpPtSxhDDosbKth9i1Pg9eqPI9jhrwZWY"
@@ -14,12 +15,37 @@ def START (bot,update):
     first_name = chat.first_name
     text = message.text
 
-    if "username" in chat:
-        username = chat.username
 
-    print(message,chat,message_id,chat_id,first_name,text,username)
+    if "username" in "{}".format(update.message.chat):
+        username = chat.username    
+
+    else:
+        username = None
+
+
+    KEYBOARD_START_1 = [
+
+       [InlineKeyboardButton('راهنما',callback_data= "sabtenam")]
+
+    ]
+    REPLY_MARKUP = InlineKeyboardMarkup(KEYBOARD_START_1)
+
+
+    bot.send_message (
+
+        chat_id = chat_id , text = "_خوش آمدید_\n\n برای شروع ابتدا برروی راهنما کلیک کنید.\n\n📢❗️📢❗️📢", parse_mode='markdown', reply_markup = REPLY_MARKUP
+
+    )
+
+def CALLBACK_DATA (bot,update):
+    
+    bot.answer_callback_query(update.callback_query.id,"لطفا شماره همراه خود را وارد کنید")
 
     
+def MESSAGE (bot,update):
+
+    pass
+
 
 
 bot.dispatcher.add_handler(
@@ -28,6 +54,15 @@ bot.dispatcher.add_handler(
 
         "start",START
 
+    )
+)
+
+
+bot.dispatcher.add_handler(
+
+    CallbackQueryHandler(
+
+        CALLBACK_DATA
     )
 )
 
